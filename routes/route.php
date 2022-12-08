f435e7c23fbff073e2d9bf34ea81b2d83adc9bad
Minhaz <minhazul.taher@gmail.com>
2022-12-08 15:33:36 +0600

    Router problem

--- routes/route.php
+++ routes/route.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Controllers\PortfoliosController;
 use Pecee\SimpleRouter\SimpleRouter;
 
 
 SimpleRouter::get('phpmvc/', function () {
   return 'Hello world';
 });
+
+
+SimpleRouter::get('phpmvc/portfolios', [PortfoliosController::class, 'index']);
