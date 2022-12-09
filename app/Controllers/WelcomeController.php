15445c39070f1ec1778d25a2dcf0ba294a522ce8
Minhaz <minhazul.taher@gmail.com>
2022-12-09 10:42:43 +0600

    add try catch for PDO connection

--- app/Controllers/WelcomeController.php
+++ app/Controllers/WelcomeController.php
@@ -9,6 +9,16 @@ class WelcomeController extends Controller
 {
     public function hello()
     {
+        // pdo connection with try catch
+        try {
+            $pdo = new \PDO('mysql:host=localhost;dbname=phpmvc;charset=utf8', 'root', '');
+        } catch (\PDOException $e) {
+            die($e->getMessage());
+        }
+
+
+
+
         return view('index');
     }
 }
